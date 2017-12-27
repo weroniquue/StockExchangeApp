2ab6fd276b5d7f5b0f04dd830b710d534d7e8786
weroniquue <weronica97@gmail.com>
2017-12-27 17:34:47 +0100

    list of company

--- src/stockapp/view/CreateIndexController.java
+++ src/stockapp/view/CreateIndexController.java
@@ -2,6 +2,7 @@ package stockapp.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
@@ -24,35 +25,47 @@ public class CreateIndexController {
 
 	@FXML
 	public void initialize() {
-		StringConverter<StockExchange> converter=new StringConverter<StockExchange>() {
+		StringConverter<StockExchange> converter = new StringConverter<StockExchange>() {
 			@Override
 			public String toString(StockExchange item) {
 				return item.getNameStockExchange();
 			}
+
 			@Override
 			public StockExchange fromString(String string) {
 				return null;
 			}
 		};
-       stockExchangeBox.setConverter(converter);
-       
-       if(stockExchangeBox.getSelectionModel().getSelectedIndex()>=0) {
-    	   companyList.setItems(main.getCompanyData());
-    	   companyList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-       }
+		stockExchangeBox.setConverter(converter);
+
+		companyList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		companyList.setCellFactory(parm -> new ListCell<Company>() {
+			@Override
+			protected void updateItem(Company item, boolean empty) {
+				super.updateItem(item, empty);
+
+				if (empty || item == null || item.getCompanyName() == null) {
+					setText("");
+				} else {
+					setText(item.getCompanyName());
+				}
+			}
+		});
 	}
 
 	public void setMain(Main main) {
 		this.main = main;
 		stockExchangeBox.setItems(main.getStockExchangeData());
+		companyList.setItems(main.getCompanyData());
+		companyList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
 	}
 
 	public void setStage(Stage stage) {
 		this.stage = stage;
 	}
-	
+
 	public void handleClose() {
-		System.out.println(stockExchangeBox.getSelectionModel().getSelectedItem().getNameStockExchange()+" "+stockExchangeBox.getSelectionModel().getSelectedItem().getAddressStockExchange());
 		stage.close();
 	}
 }
